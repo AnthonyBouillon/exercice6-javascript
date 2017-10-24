a31aa18ebba26657d9840875d80de3a698d80bce
Anthony <anthony_bouillon@outlook.fr>
2017-10-24 11:37:28 +0200

    resultat de l'exercice 6 en javascript

---
+++ exercice6.js
@@ -0,0 +1,10 @@
+// Appeller le nom de la fonction
+function donnee(){
+  // 
+  var number1 = document.getElementById('first_number').value;
+  var number2 = document.getElementById('second_number').value;
+
+  var result = number1 % number2;
+  alert(result);
+
+}
